a87d614deba2cc112151e67dba8f7fc7c3d464ef
Andrii Romaniuk <andrewrom321@gmail.com>
2024-06-18 20:57:23 +0300

    ref

---
+++ src/main/java/ua/edu/ukma/Main.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
